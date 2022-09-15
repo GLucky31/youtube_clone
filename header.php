38913eeebc2e64497a12a961a6e2e5c9c638ec78
Nik Gajšek <nikgajsek31@gmail.com>
2022-09-15 09:37:48 +0200

    fixed navbar

--- header.php
+++ header.php
@@ -6,24 +6,34 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-<header>
-    <?php
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <?php
     if(!isset($logged)){
-        echo '<a href=login.php>Login</a><br>';
-        echo '<a href=registration.php>Register</a>';
+        echo '<li class="nav-item justify-content-end"><a class="nav-link active" href=login.php>Login</a></li>';
+        echo '<li class="nav-item justify-content-end"><a class="nav-link active" href=registration.php>Register</a></li>';
     }
     else{
-        echo '<a href=logout.php>Logout</a>';
+        echo '<li class="nav-item justify-content-end"><a class="nav-link active" href=logout.php>Logout</a></li>';
     }
     ?>
+      </ul>
+    </div>
+  </div>
+</nav>
+    
 </header>
