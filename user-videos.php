c87d4da55e9956f5cf1a3efe5c814c23f3810c03
Nik Gajšek <nikgajsek31@gmail.com>
2022-10-11 09:11:09 +0200

    ye

--- user-videos.php
+++ user-videos.php
@@ -12,32 +12,51 @@ $stmt = $pdo->prepare($query);
 $stmt->execute([$_SESSION['id_user']]);
 $videos = $stmt->fetchAll();
 ?>
-
+<div class="content-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
 <div class="cb-content videolist">
     <div class="row pad-top">
         <?php
         foreach($videos as $video){
             $getID3 = new getID3;
 $file = $getID3->analyze("videos/".$video['video']);
-
-        echo "<div class='col-lg-6 col-sm-6 pad videoitem'>
+echo "<div class=' col-lg-3 col-sm-6 videoitem'>
                                 <div class='b-video'>
                                     <div class='v-img'>
                                         <a href='single-video.php?id=".$video['id_video']."'><img src='images/video1-1.png' alt=''></a>
                                         <div class='time'>".$file['playtime_string']."</div>
                                     </div>
                                     <div class='v-desc'>
-                                        <a href=href='single-video.php?id=".$video['id_video']."'>".$video['title']."</a>
+                                        <a href='single-video.php?id=".$video['id_video']."'>".$video['title']."</a>
                                     </div>
                                     <div class='v-views'>
                                         27,548 views. <span class='v-percent'><span class='v-circle'></span> 78%</span>
                                     </div>
                                 </div>
-                            </div>";}?>
+                            </div>";
+    
+                            
+                            // dropdown menu under button with ... for video with options edit and delete
+                            echo "<div class='dropdown'>
+                            <button class='btn btn-secondary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                              ...
+                            </button>
+                            <div class='dropdown-menu' aria-labelledby='dropdownMenuButton'>
+                              <a class='dropdown-item' href='upload-edit.php?id=".$video['id_video']."'>Edit</a>
+                              <a class='dropdown-item' href='video-delete.php?id=".$video['id_video']."'>Delete</a>
+                            </div>
+                            </div>";
+        
+                            }?>
 
                             
     </div>
                     
 </div>
-
+</div>
+</div>
+</div>
+</div>
 <?php include "footer.php"; ?>
